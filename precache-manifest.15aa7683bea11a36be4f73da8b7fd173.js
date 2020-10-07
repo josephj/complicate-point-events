530cd10da66e641669ec2cfd564e6708a2260012
Joseph Chiang <joseph.chiang@whitech.com.au>
2020-10-07 13:28:41 +1100

    Updates

---
+++ precache-manifest.15aa7683bea11a36be4f73da8b7fd173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8013e588139969c09eb820fd06bd05",
+    "url": "/point-events-playground/index.html"
+  },
+  {
+    "revision": "f9a1cb9fa4bf488206b4",
+    "url": "/point-events-playground/static/css/main.89d83d6d.chunk.css"
+  },
+  {
+    "revision": "50b367a063253a3671ea",
+    "url": "/point-events-playground/static/js/2.a25ad851.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/point-events-playground/static/js/2.a25ad851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a1cb9fa4bf488206b4",
+    "url": "/point-events-playground/static/js/main.42337fbc.chunk.js"
+  },
+  {
+    "revision": "b8fc3dc9b778f7b6c947",
+    "url": "/point-events-playground/static/js/runtime-main.8bf16e3c.js"
+  }
+]);
