97da9c38ca875ff43fe0dfa53c3d8eea90edbc51
Joseph Chiang <joseph.chiang@whitech.com.au>
2020-10-08 15:31:09 +1100

    Updates

---
+++ precache-manifest.5c888cc89ed7d3a26fc1bf2963e1b824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a59c9b5cb81cd8654a45c94cd319fd",
+    "url": "/point-events-prototype/index.html"
+  },
+  {
+    "revision": "7c26a6745384c2de9380",
+    "url": "/point-events-prototype/static/css/main.e14a422d.chunk.css"
+  },
+  {
+    "revision": "f7ebd641817a4bcd1444",
+    "url": "/point-events-prototype/static/js/2.8e322a01.chunk.js"
+  },
+  {
+    "revision": "69a4e3231f74c130443edc90bab82408",
+    "url": "/point-events-prototype/static/js/2.8e322a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c26a6745384c2de9380",
+    "url": "/point-events-prototype/static/js/main.5bce4d24.chunk.js"
+  },
+  {
+    "revision": "84721a0c928d286847ea",
+    "url": "/point-events-prototype/static/js/runtime-main.1f80e463.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/point-events-prototype/precache-manifest.334c76b1b1f7688118260131f57cf373.js"
+  "/point-events-prototype/precache-manifest.5c888cc89ed7d3a26fc1bf2963e1b824.js"
 );
 
 self.addEventListener('message', (event) => {
