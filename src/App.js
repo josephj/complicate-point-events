465fde53f5a7c33cd3c7de64c1b561977d8ef5ed
Joseph Chiang <joseph.chiang@whitech.com.au>
2020-10-09 11:32:47 +1100

    updates according to the feedbacks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Item from "./Item";
+import "pepjs";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Pointer Events</h1>
-      <p>Scrolling, Item Selection, and Crop Moving</p>
-      <ul>
-        {Array.from(Array(50)).map((_, i) => (
-          <Item key={i} id={i} />
-        ))}
-      </ul>
+      <div className="Wrapper">
+        <h1>Pointer Events</h1>
+        <p>Scrolling, Item Selection, and Crop Moving (150ms)</p>
+        <ul>
+          {Array.from(Array(50)).map((_, i) => (
+            <Item key={`Item-${i}`} id={i} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/Crop.js
+++ src/Crop.js
@@ -1,108 +1,129 @@
 import cx from "classnames";
-import React, { useRef, useState } from "react";
-import "pepjs";
-// import useLongPress from "./useLongPress";
+import React, { useRef, useState, useEffect } from "react";
 
-document.addEventListener("pointercancel", (e) => {
-  console.log("handleDocumentPointerCancel");
-});
+const CROP_DELAY = 150;
 
 let timer;
 let labelStyle = `background: #369; color: #fff; padding: 2px 5px; width: 100px; border-radius: 4px; display: inline-block`;
-const Crop = () => {
+let errorStyle = `background: #c00; color: #fff; padding: 2px 5px; width: 100px; border-radius: 4px; display: inline-block`;
+
+document.addEventListener("pointercancel", (e) => {
+  console.log(
+    `%c${e.type}`,
+    errorStyle,
+    "document pointercancel is triggered!"
+  );
+});
+
+const Crop = ({ isLandscape }) => {
   const ref = useRef(null);
   const [isMoving, setIsMoving] = useState(false);
   const [posX, setPosX] = useState(null);
+  const [posY, setPosY] = useState(null);
 
-  const handlePointerDown = (e) => {
-    console.log(`%c${e.type}`, labelStyle, `e.pressure = ${e.pressure}`);
-    setPosX(e.clientX);
+  useEffect(() => {
     const el = ref.current;
-    timer = setTimeout(() => {
-      // callback
-      setIsMoving(true);
-      el.style.touchAction = "none";
-      el.closest("ul").style.touchAction = "none";
-      el.closest(".Item ").style.touchAction = "none";
-      console.log("e", e.type); // FIXME - Why?
-      console.log(`%c${e.type}`, labelStyle, "touchAction", "none");
-      timer = null;
-    }, 500);
-  };
+    const appEl = el.closest(".App");
 
-  const handlePointerUp = (e) => {
-    if (timer) {
-      clearTimeout(timer);
-      console.log(`%c${e.type}`, labelStyle, `Failed to long press`);
-    } else {
-      // success long press
-      const el = ref.current;
-      el.style.touchAction = "";
-      el.closest(".Item ").style.touchAction = "";
-      el.closest("ul").style.touchAction = "";
-      console.log(`%c${e.type}`, labelStyle, "Long pressed successfully");
-      // console.log(e.type, "touchAction", el.closest(".Item").style.touchAction);
-    }
-    setIsMoving(false);
-  };
+    const handlePointerDown = (e) => {
+      console.log(`%c${e.type}`, labelStyle);
+      if (isLandscape) setPosX(e.clientX);
+      else setPosY(e.clientY);
+      timer = setTimeout(() => {
+        appEl.style.touchAction = "none";
+        setIsMoving(true);
+        if (!e.type)
+          console.log(`%chandlePointerDown`, errorStyle, `e.type = `, e.type);
+        timer = null;
+      }, CROP_DELAY);
+    };
 
-  const handlePointerMove = (e) => {
-    // console.log(`%c${e.type}`, labelStyle);
-    if (!isMoving) return;
-    // console.log(`%c${e.type}`, labelStyle, "isMoving");
-    const el = e.currentTarget;
-    const parentEl = el.parentNode;
-    const maxMoveX = parentEl.offsetWidth - el.offsetWidth - 8;
-    const moveX = posX - e.clientX;
-    const left = Math.min(Math.max(0, el.offsetLeft - moveX), maxMoveX);
-    el.style.top = 0;
-    el.style.left = `${left}px`;
-    console.log(
-      `%c${e.type}`,
-      labelStyle,
-      "left",
-      left,
-      "e.clientX",
-      e.clientX
-    );
-    setPosX(e.clientX);
-  };
+    const handlePointerUp = (e) => {
+      if (timer) {
+        clearTimeout(timer);
+        console.log(`%c${e.type}`, labelStyle, `Failed to long press`);
+      } else {
+        console.log(`%c${e.type}`, labelStyle, "Long pressed successfully");
+      }
+      appEl.style.touchAction = "";
+      setIsMoving(false);
+    };
 
-  const handlePointerOut = (e) => {
-    console.log(`%c${e.type}`, labelStyle);
-  };
+    const handlePointerMove = (e) => {
+      if (!isMoving) return;
+      e.preventDefault();
+      const parentEl = el.parentNode;
+      const maxMove = isLandscape
+        ? parentEl.offsetWidth - el.offsetWidth - 8
+        : parentEl.offsetHeight - el.offsetHeight - 8;
+      const move = isLandscape ? posX - e.clientX : posY - e.clientY;
+      const offset = isLandscape ? el.offsetLeft : el.offsetTop;
+      const position = Math.min(Math.max(0, offset - move), maxMove);
+      if (isLandscape) {
+        el.style.left = `${position}px`;
+        setPosX(e.clientX);
+      } else {
+        el.style.top = `${position}px`;
+        setPosY(e.clientY);
+      }
+      console.log(`%c${e.type}`, labelStyle, "movement", position);
+    };
 
-  const handlePointerLeave = (e) => {
-    console.log(`%c${e.type}`, labelStyle);
-  };
+    const handlePointerOut = (e) => {
+      console.log(`%c${e.type}`, labelStyle);
+    };
 
-  const handleGotPointerCapture = (e) => {
-    console.log(`%c${e.type}`, labelStyle);
-  };
+    const handlePointerLeave = (e) => {
+      console.log(`%c${e.type}`, labelStyle);
+      appEl.style.touchAction = "";
+      setIsMoving(false);
+    };
 
-  const handleLostPointerCapture = (e) => {
-    console.log(`%c${e.type}`, labelStyle);
-  };
+    const handleGotPointerCapture = (e) => {
+      console.log(`%c${e.type}`, labelStyle);
+    };
 
-  const handlePointerCancel = (e) => {
-    console.log(`%c${e.type}`, labelStyle);
-    if (timer) clearTimeout(timer);
-    setPosX(null);
-    setIsMoving(false);
-  };
+    const handleLostPointerCapture = (e) => {
+      console.log(`%c${e.type}`, labelStyle);
+    };
+
+    const handlePointerCancel = (e) => {
+      console.log(`%c${e.type}`, errorStyle, e.target.className);
+      e.stopPropagation();
+      if (timer) clearTimeout(timer);
+      if (isLandscape) setPosX(null);
+      else setPosY(null);
+      appEl.style.touchAction = "";
+      setIsMoving(false);
+    };
+    el.addEventListener("pointerdown", handlePointerDown);
+    el.addEventListener("pointerup", handlePointerUp);
+    el.addEventListener("pointermove", handlePointerMove);
+    el.addEventListener("pointercancel", handlePointerCancel);
+    el.addEventListener("pointerout", handlePointerOut);
+    el.addEventListener("pointerleave", handlePointerLeave);
+    el.addEventListener("gotpointercapture", handleGotPointerCapture);
+    el.addEventListener("lostpointercapture", handleLostPointerCapture);
+    return () => {
+      el.removeEventListener("pointerdown", handlePointerDown);
+      el.removeEventListener("pointerup", handlePointerUp);
+      el.removeEventListener("pointermove", handlePointerMove);
+      el.removeEventListener("pointercancel", handlePointerCancel);
+      el.removeEventListener("pointerout", handlePointerOut);
+      el.removeEventListener("pointerleave", handlePointerLeave);
+      el.removeEventListener("gotpointercapture", handleGotPointerCapture);
+      el.removeEventListener("lostpointercapture", handleLostPointerCapture);
+    };
+  }, [isMoving, posX, posY, isLandscape]);
 
   return (
     <span
       ref={ref}
-      className={cx("Crop", { "Crop--moving": isMoving })}
-      onPointerDown={handlePointerDown}
-      onPointerUp={handlePointerUp}
-      onPointerMove={handlePointerMove}
-      onPointerCancel={handlePointerCancel}
-      onPointerOut={handlePointerOut}
-      onPointerLeave={handlePointerLeave}
-      onGotPointerCapture={handleGotPointerCapture}
-      onLostPointerCapture={handleLostPointerCapture}
+      className={cx("Crop", {
+        "Crop-portrait": !isLandscape,
+        "Crop-landscape": isLandscape,
+        "Crop--moving": isMoving,
+      })}
       onContextMenu={(e) => e.preventDefault()}
     ></span>
   );
